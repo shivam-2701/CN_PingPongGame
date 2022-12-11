093d07704cb8f42620e87039dbea3211bfc65f39
shivam-2701 <shivam.shah2701@gmail.com>
2022-12-11 23:03:08 +0530

    Working Style

---
+++ script.js
@@ -0,0 +1,9 @@
+let bar1= document.getElementById('rod-one');
+let bar2= document.getElementById('rod-two');
+let ball = document.querySelector(".ball");
+
+function initializeGame(){
+    bar1.style.left="42%";
+    bar2.style.left="42%";
+    ball.style.left="50%";
+}
