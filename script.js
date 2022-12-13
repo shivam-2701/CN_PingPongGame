51b98e61d878fff6e2c789ff0affa4d43b72ac4e
shivam-2701 <shivam.shah2701@gmail.com>
2022-12-14 00:08:04 +0530

    Project finished

--- script.js
+++ script.js
@@ -9,14 +9,35 @@ let yDir = -1;
 let xSpd = 4;
 let ySpd = 5;
 
+const PLAYER_ONE_KEY = "play_one_max";
+const PLAYER_TWO_KEY = "play_two_max";
 
-let initialPos={
-   left:(window.innerWidth / 2),
-   right:ball.getBoundingClientRect().right,
-   top:innerHeight - bar2.offsetHeight - ball.offsetHeight,
-   bottom:ball.getBoundingClientRect().bottom
+
+let initialPos = {
+    left: (window.innerWidth / 2),
+    right: ball.getBoundingClientRect().right,
+    top: innerHeight - bar2.offsetHeight - ball.offsetHeight,
+    bottom: ball.getBoundingClientRect().bottom
 }
 let score1 = 0, score2 = 0, maxScore1 = 0, maxScore2 = 0;
+
+// Getting max score from localStorage
+
+if (localStorage.getItem(PLAYER_ONE_KEY) != undefined) {
+    console.log("localStorage.getItem");
+    maxScore1 = parseInt(localStorage.getItem(PLAYER_ONE_KEY));
+    console.log("maxScore1: " + maxScore1);
+}
+
+if (localStorage.getItem(PLAYER_TWO_KEY) != undefined) {
+    console.log("localStorage.getItem");
+    maxScore2 = parseInt(localStorage.getItem(PLAYER_TWO_KEY));
+    console.log("maxScore2: " + maxScore2);
+}
+
+
+
+
 
 // Variable to store the intervalId of the game engine
 let gameId = null;
@@ -25,13 +46,40 @@ function setSpeed() {
     ySpd = Math.floor(Math.random() * 4) + 3;
 }
 
+function addEventListenerToRods() {
+    window.addEventListener("keydown", function (event) {
+        let key = event.key;
+        if (key === 'd' || key === 'D') {
+            let left_numeric = parseInt(
+                bar1.style.left.substring(0, bar1.style.left.length - 2)
+            );
+            left_numeric += 20;
+            if (left_numeric + bar1.offsetWidth > document.documentElement.clientWidth) {
+                left_numeric = document.documentElement.clientWidth - bar1.offsetWidth;
+            }
+            bar1.style.left = left_numeric.toString() + "px";
+            bar2.style.left = left_numeric.toString() + "px";
+        } else if (key === 'a' || key === 'A') {
+            let left_numeric = parseInt(
+                bar1.style.left.substring(0, bar1.style.left.length - 2)
+            );
+            left_numeric -= 20;
+            if (left_numeric < 0) {
+                left_numeric = 0;
+            }
+            bar1.style.left = left_numeric.toString() + "px";
+            bar2.style.left = left_numeric.toString() + "px";
+        }
+    });
+}
+
 
 function resetGamePos() {
     setSpeed();
     bar1.style.left = (window.innerWidth / 2 - bar1.offsetWidth / 2) + "px";
     bar2.style.left = (window.innerWidth / 2 - bar1.offsetWidth / 2) + "px";
     bar2.style.top = window.innerHeight - bar2.offsetHeight + "px";
-    ball.style.top= initialPos.top +"px";
+    ball.style.top = initialPos.top + "px";
     ball.style.left = initialPos.left + "px";
     console.log("Game Pos reset");
     if (gameId != null) {
@@ -39,10 +87,8 @@ function resetGamePos() {
     }
 }
 function initializeGame() {
-    console.log(initialPos,ball.getBoundingClientRect());
+    console.log(initialPos, ball.getBoundingClientRect());
     resetGamePos();
-    addEventListenerToRods();
-    score1 = 0, score2 = 0, maxScore1 = 0, maxScore2 = 0;
 }
 function startGame() {
     gameId = setInterval(() => {
@@ -81,20 +127,19 @@ function hitBar(ball, bar) {
 
 function checkTopCollision() {
     let ballPos = ball.getBoundingClientRect();
-    if (ballPos.top > 0) {
+    if (ballPos.top > bar1.getBoundingClientRect().bottom) {
         //Collided with bottom
         if (hitBar(ball, bar2)) {
             yDir = (yDir == -1 ? 1 : -1);
-
         } else {
             score1++;
             maxScore1 = Math.max(maxScore1, score1);
-            console.log("score1 " + score1, maxScore1);
+            localStorage.setItem(PLAYER_ONE_KEY, maxScore1.toString());
             clearInterval(gameId);
             gameId = null;
             resetGamePos();
-            window.alert("Player 1 Lost!! \n Press Enter to continue");
-
+            window.alert(`Player 1 Won ${score1} Max Score: ${maxScore1} \n Press Enter to continue`);
+            startGame();
         }
     } else {
 
@@ -104,45 +149,25 @@ function checkTopCollision() {
 
         } else {
             score2++;
-            maxScore1 = Math.max(maxScore2, score2);
+            maxScore2 = Math.max(maxScore2, score2);
             clearInterval(gameId);
+            gameId = null;
             resetGamePos();
-            console.log("score2 " + score2, maxScore2);
-            window.alert("Player 2 Lost!! \n Press Enter to continue");
+            localStorage.setItem(PLAYER_TWO_KEY, maxScore2.toString());
+            window.alert(`Player 2 Won ${score2} Max Score: ${maxScore2} \n Press Enter to continue`);
+            startGame();
         }
     }
 }
-function addEventListenerToRods() {
-    window.addEventListener("keydown", function (event) {
-        console.log("key Pressed", event.key);
-        bar2 = bar2;
-        let key = event.key;
-        if (key === 'd' || key === 'D') {
-
-            let left_numeric = parseInt(
-                bar1.style.left.substring(0, bar1.style.left.length - 2)
-            );
-            left_numeric += 20;
-            if (left_numeric + bar1.offsetWidth > document.documentElement.clientWidth) {
-                left_numeric = document.documentElement.clientWidth - bar1.offsetWidth;
-            }
-            bar1.style.left = left_numeric.toString() + "px";
-            bar2.style.left = left_numeric.toString() + "px";
-        } else if (key === 'a' || key === 'A') {
-            let left_numeric = parseInt(
-                bar1.style.left.substring(0, bar1.style.left.length - 2)
-            );
-            left_numeric -= 20;
-            if (left_numeric < 0) {
-                left_numeric = 0;
-            }
-            bar1.style.left = left_numeric.toString() + "px";
-            bar2.style.left = left_numeric.toString() + "px";
-        }
-    });
-}
 
 
+
+
+alert("Press Enter to startGame \n Press Escape to stop game");
+
+initializeGame();
+addEventListenerToRods();
+gameId = startGame();
 window.addEventListener('keydown', function (e) {
     if (e.key === 'Escape') {
         clearInterval(gameId);
@@ -152,10 +177,5 @@ window.addEventListener('keydown', function (e) {
             clearInterval(gameId);
         }
         resetGamePos();
-        // gameId = startGame();
     }
 });
-alert("Press Enter to startGame");
-
-initializeGame();
-gameId = startGame();
